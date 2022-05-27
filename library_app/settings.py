709a79445eb55d224159bf5273dfc52c762a3c91
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-05-27 09:29:15 -0500

    Changed secret key and moved it into .env

--- library_app/settings.py
+++ library_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dzf935!nk)*q30d#dw*9#+%!&ac(7v9lx*ajqu0$l3699tcxfq'
+# SECRET_KEY = 'django-insecure-dzf935!nk)*q30d#dw*9#+%!&ac(7v9lx*ajqu0$l3699tcxfq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
