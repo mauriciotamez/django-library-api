18f4de3fb6cd4c4c7c9448b9c5d3962e33308281
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-06-14 03:07:32 -0500

    added basic testing for user login

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+import pytest
+from rest_framework.test import APIClient
 
-# Create your tests here.
+client = APIClient()
+
+
+@pytest.mark.django_db
+def test_register_user():
+    payload = dict(
+        email="usertest@example.com",
+        password="123test",
+        first_name="Bot",
+        last_name="Test"
+    )
+
+    response = client.post("/api/users/", payload)
+
+    data = response.data
+
+    assert data["email"] == payload["email"]
+    assert "password" not in data
+    assert data["first_name"] == payload["first_name"]
+    assert data["last_name"] == payload["last_name"]
+
+
+@pytest.mark.django_db
+def test_login_user():
+    payload = dict(
+        email="usertest@example.com",
+        password="123test",
+        first_name="Bot",
+        last_name="Test"
+    )
+
+    client.post("/api/users/", payload)
+
+    response = client.post(
+        "/api/token/", dict(email="usertest@example.com", password="123test"))
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_login_user_fail():
+    response = client.post(
+        "/api/token/", dict(email="usertest@example.com", password="123test123"))
+
+    assert response.status_code == 401
